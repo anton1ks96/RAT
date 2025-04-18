800f5a10a33aefae88427ff65355f7221e605984
i24s0024 <i24s0024@students.it-college.ru>
2025-04-18 15:09:54 +0500

    Update SystemPrompt
    Снова обновлён системный промпт

--- interface/telegram_interface.py
+++ interface/telegram_interface.py
@@ -54,7 +54,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             try:
                 save_to_google_sheets(order_data)
                 logging.info("Сохранение заявки в google sheets.")
-                await update.message.reply_text("Ваша завявка была собрана и отправлена!")
+                await update.message.reply_text("Ваша заявка была собрана и отправлена!")
             except Exception as e:
                 logging.info(f"Ошибка при сохранении в google sheets: {e}")
 
